6ef818ca0102626077d2a17e9487c22c0bbbd717
hwoo <tone8943@gmail.com>
2025-03-23 20:07:43 +0900

    Initial commit

---
+++ src/main/java/com/example/raghotel/RaghotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.raghotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaghotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaghotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/raghotel/RaghotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaghotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
